87d506cb3debfc269fb0fd9ad832db22beb387d5
errormin <smgm030@gmail.com>
2021-12-17 20:24:17 +0900

    first commit

---
+++ 06_bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "vmss_bs_sub" {
+  name                = "sm_pub"
+  location            = azurerm_resource_group.bespin.location
+  resource_group_name = azurerm_resource_group.bespin.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "vmss_bsh_host" {
+  name                = "sm_bsh_host"
+  location            = azurerm_resource_group.bespin.location
+  resource_group_name = azurerm_resource_group.bespin.name
+
+  ip_configuration {
+    name                 = "AzureBastionHost"
+    subnet_id            = azurerm_subnet.bs_sub.id
+    public_ip_address_id = azurerm_public_ip.vmss_bs_sub.id
+  }
+}
