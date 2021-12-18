e786285a208fe54eb231d43ae6eb34dfd090ec54
errormin <smgm030@gmail.com>
2021-12-18 20:55:34 +0900

    2021-12-18 stap2

--- 02_vnet.tf
+++ 02_vnet.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_network" "bespin" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "vmss-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.bespin.location
-  resource_group_name = azurerm_resource_group.bespin.name
+  location            = azurerm_resource_group.Azure1_rg.location
+  resource_group_name = azurerm_resource_group.Azure1_rg.name
   tags                = var.tags # (3)
 }
 #===================================================
