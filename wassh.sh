b5233e8ec2e0560ba9beb44fe08a1b532c7bce2e
errormin <smgm030@gmail.com>
2021-12-20 18:39:21 +0900

    2021-12-20 stap5 wassh

--- wassh.sh
+++ wassh.sh
@@ -7,7 +7,7 @@ ln -s /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 cat > .vimrc << EOF
 set paste
 EOF
-yum install -y wget mysql java-11-openjdk-devel.x86_64
+yum install -y wget mysql java-11-openjdk-devel.x86_64 git
 cat >> /etc/profile << \END_HELP
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.13.0.8-1.el7_9.x86_64
 export PATH=$PATH:$JAVA_HOME/bin
@@ -31,8 +31,6 @@ sed -i '19 i\<!--' /usr/local/tomcat9/webapps/manager/META-INF/context.xml
 sed -i '24 i\-->' /usr/local/tomcat9/webapps/manager/META-INF/context.xml
 sed -i '19 i\<!--' /usr/local/tomcat9/webapps/host-manager/META-INF/context.xml
 sed -i '24 i\-->' /usr/local/tomcat9/webapps/host-manager/META-INF/context.xml
-cd /usr/local/tomcat9/bin
-./startup.sh
 cd /usr/local
 wget http://mirror.apache-kr.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar zxvf apache-maven-3.6.3-bin.tar.gz
@@ -44,18 +42,16 @@ export PATH=$PATH:$HOME/bin:$MAVEN_HOME/bin
 export TOMCAT_HOME=/usr/local/tomcat9
 export RESOURCEGROUP_NAME=azure1-3tier-terraform
 export MYSQL_SERVER_NAME=smterrordb
-export MYSQL_SERVER_FULL_NAME=${MYSQL_SERVER_NAME}.mysql.database.azure.com
+export MYSQL_SERVER_FULL_NAME=${MYSQL_SERVER_NAME}.privatelink.mysql.database.azure.com
 export MYSQL_SERVER_ADMIN_LOGIN_NAME=azureuser
-export MYSQL_SERVER_ADMIN_PASSWORD=!Rlflqhdl21
+export MYSQL_SERVER_ADMIN_PASSWORD=#Rlflqhdl21
 export MYSQL_DATABASE_NAME=petclinic
 export DOLLAR=\$
 END_HELP
 source /etc/profile
-yum install -y git
 cd /usr/local
 git clone https://github.com/spring-petclinic/spring-framework-petclinic.git
 cd spring-framework-petclinic
-mvn package
 cat > cargo.xml << \END_HELP
 <plugin>
 <groupId>org.codehaus.cargo</groupId>
@@ -84,11 +80,39 @@ cat > cargo.xml << \END_HELP
 </configuration>
 </plugin>
 END_HELP
+cat > mysql.xml << \END_HELP
+<profile>
+<id>MySQL</id>
+<activation>
+<activeByDefault>true</activeByDefault>
+</activation>
+<properties>
+<db.script>mysql</db.script>
+<jpa.database>MYSQL</jpa.database>
+<jdbc.driverClassName>com.mysql.jdbc.Driver</jdbc.driverClassName>
+<jdbc.url>jdbc:mysql://${DOLLAR}{MYSQL_SERVER_FULL_NAME}:3306/${DOLLAR}{MYSQL_DATABASE_NAME}?useUnicode=true</jdbc.url>
+<jdbc.username>${DOLLAR}{MYSQL_SERVER_ADMIN_LOGIN_NAME}@${DOLLAR}{MYSQL_SERVER_FULL_NAME}</jdbc.username>
+<jdbc.password>${DOLLAR}{MYSQL_SERVER_ADMIN_PASSWORD}</jdbc.password>
+</properties>
+<dependencies>
+<dependency>
+<groupId>mysql</groupId>
+<artifactId>mysql-connector-java</artifactId>
+<version>${mysql-driver.version}</version>
+<scope>runtime</scope>
+</dependency>
+</dependencies>
+</profile>
+END_HELP
 sed -i '461r cargo.xml' pom.xml
-rm -rf cargo.xml
+mvn package
+mvn cargo:deploy
+sed -i '495,497d' pom.xml
+sed -i '531,549d' pom.xml
+sed -i '530r mysql.xml' pom.xml
+rm -rf cargo.xml mysql.xml
+mvn package
 mvn cargo:deploy
 cd target
-cp petclinic /usr/local/tomcat9/webapps/
 cp petclinic.war /usr/local/tomcat9/webapps/
-${TOMCAT_HOME}/bin/shutdown.sh
 ${TOMCAT_HOME}/bin/startup.sh
