b7663569042341f043dbff8609739d9ac8ec791c
errormin <smgm030@gmail.com>
2021-12-21 18:30:52 +0900

    2021-12-21 stap6

--- websh.sh
+++ websh.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-setenforce 0
+sudo su -
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-systemctl disable --now firewalld
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+firewall-cmd --permanent --add-port=443/tcp
+firewall-cmd --reload
+firewall-cmd --permanent --add-port=22/tcp
+firewall-cmd --reload
 rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 cat > .vimrc << EOF
@@ -14,21 +19,21 @@ wget http://archive.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connector
 tar zxvf tomcat-connectors-1.2.46-src.tar.gz
 rm -rf tomcat-connectors-1.2.46-src.tar.gz
 cd tomcat-connectors-1.2.46-src/native
-./configure --with-apxs=/bin/apxs
+/bin/bash ./configure --with-apxs=/bin/apxs
 make
 make install
 cd apache-2.0/
 cp mod_jk.so /usr/lib64/httpd/modules/mod_jk.so
 chmod 755 /usr/lib64/httpd/modules/mod_jk.so
 echo 'LoadModule jk_module /usr/lib64/httpd/modules/mod_jk.so' >> /etc/httpd/conf/httpd.conf
-echo '<IfModule jk_module>' >> /etc/httpd/conf/httpd.conf
-echo '  JkWorkersFile conf/workers.properties' >> /etc/httpd/conf/httpd.conf
-echo '  JkLogFile logs/mod_jk.log' >> /etc/httpd/conf/httpd.conf
-echo '  JkShmFile run/mod_jk.shm' >> /etc/httpd/conf/httpd.conf
-echo '  JkLogLevel info' >> /etc/httpd/conf/httpd.conf
-echo '  JkLogStampFormat "[%a %b %d %H:%M:%S %Y]"' >> /etc/httpd/conf/httpd.conf
-echo '  JkMount /* worker1' >> /etc/httpd/conf/httpd.conf
-echo '</IfModule>' >> /etc/httpd/conf/httpd.conf
+echo '     <IfModule jk_module>' >> /etc/httpd/conf/httpd.conf
+echo '     	JkWorkersFile conf/workers.properties' >> /etc/httpd/conf/httpd.conf
+echo '     	JkLogFile logs/mod_jk.log' >> /etc/httpd/conf/httpd.conf
+echo '     	JkShmFile run/mod_jk.shm' >> /etc/httpd/conf/httpd.conf
+echo '     	JkLogLevel info' >> /etc/httpd/conf/httpd.conf
+echo '     	JkLogStampFormat "[%a %b %d %H:%M:%S %Y]"' >> /etc/httpd/conf/httpd.conf
+echo '     	JkMount /* worker1' >> /etc/httpd/conf/httpd.conf
+echo '     </IfModule>' >> /etc/httpd/conf/httpd.conf
 cat > /etc/httpd/conf/workers.properties << EOF
 worker.list=worker1
 worker.worker1.type=ajp13
