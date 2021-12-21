ef452254aff3e3db42b5dbdcae908c47502861bb
errormin <smgm030@gmail.com>
2021-12-21 18:29:20 +0900

    2021-12-21 stap6

--- 07_jumpbox_vm.tf
+++ 07_jumpbox_vm.tf
@@ -1,32 +1,8 @@
-resource "azurerm_public_ip" "jumpbox" {
-  name                = "jumpbox-public-ip"
-  location            = var.location # (2)
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
-  allocation_method   = "Static"
-  domain_name_label   = "${random_string.fqdn.result}-ssh"
-  tags                = var.tags # (3)
-}
-
-resource "azurerm_network_interface" "jumpbox" {
-  name                = "jumpbox-nic"
-  location            = var.location # (2)
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
-
-  ip_configuration {
-    name                          = "IPConfiguration"
-    subnet_id                     = azurerm_subnet.JumpBox_subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.jumpbox.id
-  }
-
-  tags = var.tags
-}
-
-resource "azurerm_virtual_machine" "jumpbox" {
-  name                  = "jumpbox"
+resource "azurerm_virtual_machine" "smlee_jumpbox" {
+  name                  = "smlee-jumpbox"
   location              = var.location # (2)
-  resource_group_name   = azurerm_resource_group.Azure1_rg.name
-  network_interface_ids = [azurerm_network_interface.jumpbox.id]
+  resource_group_name   = azurerm_resource_group.smlee_rg.name
+  network_interface_ids = [azurerm_network_interface.smlee_jumpbox_nic.id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
