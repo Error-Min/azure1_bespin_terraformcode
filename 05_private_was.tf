9c84ae0ec049f23e4a2c386481a72d8a82cd0dbb
errormin <smgm030@gmail.com>
2021-12-21 18:28:46 +0900

    2021-12-21 stap6

--- 05_private_was.tf
+++ 05_private_was.tf
@@ -1,44 +1,44 @@
-resource "azurerm_lb" "was_vmss" {
-  name                = "was-vmss-lb"
+resource "azurerm_lb" "smlee_was_lb" {
+  name                = "smlee-was-lb"
   location            = var.location # (2)
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
+  resource_group_name = azurerm_resource_group.smlee_rg.name
 
   frontend_ip_configuration {
     name                          = "wasPrivateIP"
-    subnet_id                     = azurerm_subnet.WAS_LB_Subnet.id
+    subnet_id                     = azurerm_subnet.smlee_lb_subnet.id
     private_ip_address            = "10.0.2.5"
     private_ip_address_allocation = "Static"
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "wasbpepool" { # 로벨 백엔드 풀
-  loadbalancer_id = azurerm_lb.was_vmss.id
+resource "azurerm_lb_backend_address_pool" "smlee_was_bp" { # 로벨 백엔드 풀
+  loadbalancer_id = azurerm_lb.smlee_was_lb.id
   name            = "WasBackEndAddressPool"
 }
 
-resource "azurerm_lb_probe" "wasvmss" { # 로벨 프로브
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
-  loadbalancer_id     = azurerm_lb.was_vmss.id
+resource "azurerm_lb_probe" "smlee_waslb_probe" { # 로벨 프로브
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  loadbalancer_id     = azurerm_lb.smlee_was_lb.id
   name                = "was-running-probe"
   port                = 8009
 }
 
-resource "azurerm_lb_rule" "waslbnatrule" { # 부하분산 규칙 추가
-  resource_group_name            = azurerm_resource_group.Azure1_rg.name
-  loadbalancer_id                = azurerm_lb.was_vmss.id # NAT 규칙을 생성할 LoadBalancer의 ID
+resource "azurerm_lb_rule" "smlee_waslb_rule" { # 부하분산 규칙 추가
+  resource_group_name            = azurerm_resource_group.smlee_rg.name
+  loadbalancer_id                = azurerm_lb.smlee_was_lb.id # NAT 규칙을 생성할 LoadBalancer의 ID
   name                           = "was_ilb_rule"
   protocol                       = "Tcp"
   frontend_port                  = 8009 # (4) 80번 포트 외부
   backend_port                   = 8009
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.wasbpepool.id #백엔드 풀 추가
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.smlee_was_bp.id #백엔드 풀 추가
   frontend_ip_configuration_name = "wasPrivateIP"                                   # 규칙이 연결된 프런트엔드 IP 구성의 이름
-  probe_id                       = azurerm_lb_probe.wasvmss.id                   #상태 프로브 추가
+  probe_id                       = azurerm_lb_probe.smlee_waslb_probe.id                   #상태 프로브 추가
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
-  name                            = "sm-was-vmss"
-  location                        = azurerm_resource_group.Azure1_rg.location # (2)
-  resource_group_name             = azurerm_resource_group.Azure1_rg.name
+resource "azurerm_linux_virtual_machine_scale_set" "smlee_was_vmss" {
+  name                            = "smlee-was-vmss"
+  location                        = azurerm_resource_group.smlee_rg.location # (2)
+  resource_group_name             = azurerm_resource_group.smlee_rg.name
   sku                             = "Standard_DS1_v2" # 머신 디스크 크기 선택 및 vmss 개수 지정 
   instances                       = 3                 # vmss 가상머신 개수.
   admin_username                  = var.admin_user
@@ -75,8 +75,8 @@ resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
 
     ip_configuration {
       name                                   = "IPConfiguration"
-      subnet_id                              = azurerm_subnet.WAS_Vmss_Subnet.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.wasbpepool.id]
+      subnet_id                              = azurerm_subnet.smlee_was_subnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.smlee_was_bp.id]
       primary                                = true
     }
   }
@@ -103,11 +103,11 @@ resource "azurerm_linux_virtual_machine_scale_set" "wasvmss" {
 }
 
 
-resource "azurerm_monitor_autoscale_setting" "was_auto_scale" {
+resource "azurerm_monitor_autoscale_setting" "smlee_was_auto_scale" {
   name                = "was-autoscale-config"
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
-  location            = azurerm_resource_group.Azure1_rg.location
-  target_resource_id  = azurerm_linux_virtual_machine_scale_set.wasvmss.id
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  location            = azurerm_resource_group.smlee_rg.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.smlee_was_vmss.id
 
   profile {
     name = "AutoScale"
@@ -121,13 +121,13 @@ resource "azurerm_monitor_autoscale_setting" "was_auto_scale" {
     rule {
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.wasvmss.id
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.smlee_was_vmss.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
         time_aggregation   = "Average"
         operator           = "GreaterThan"
-        threshold          = 60
+        threshold          = 70
       }
 
       scale_action {
@@ -141,13 +141,13 @@ resource "azurerm_monitor_autoscale_setting" "was_auto_scale" {
     rule {
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.wasvmss.id
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.smlee_was_vmss.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
         time_aggregation   = "Average"
         operator           = "LessThan"
-        threshold          = 20
+        threshold          = 40
       }
 
       scale_action {
