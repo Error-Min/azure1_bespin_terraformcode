bbccf416774cd41e3742db8a302a5d78ab2e4862
errormin <smgm030@gmail.com>
2021-12-21 18:28:55 +0900

    2021-12-21 stap6

--- 06_bastion.tf
+++ 06_bastion.tf
@@ -1,19 +1,19 @@
 resource "azurerm_public_ip" "vmss_bs_sub" {
-  name                = "sm_pub"
-  location            = azurerm_resource_group.Azure1_rg.location
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
+  name                = "sm-pub"
+  location            = azurerm_resource_group.smlee_rg.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "vmss_bsh_host" {
   name                = "sm_bsh_host"
-  location            = azurerm_resource_group.Azure1_rg.location
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
+  location            = azurerm_resource_group.smlee_rg.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
 
   ip_configuration {
     name                 = "AzureBastionHost"
-    subnet_id            = azurerm_subnet.bs_sub.id
+    subnet_id            = azurerm_subnet.bs_subnet.id
     public_ip_address_id = azurerm_public_ip.vmss_bs_sub.id
   }
 }
