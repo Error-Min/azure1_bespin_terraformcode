6c085c1600ae085e861aede84bfa4509fc69e32e
errormin <smgm030@gmail.com>
2021-12-21 18:28:28 +0900

    2021-12-21 stap6

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -1,56 +1,55 @@
 # WEB LB subnet
-resource "azurerm_subnet" "WEB_LB_Subnet" {
-  name                 = "web-lb-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
-  address_prefixes     = ["10.0.1.0/29"]
+resource "azurerm_subnet" "smlee_appgw_subnet" {
+  name                 = "smlee-appgw-subnet"
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
+  address_prefixes     = ["10.0.1.0/28"]
 }
 
 # WEB vmss subnet
-resource "azurerm_subnet" "web_vmss" {
-  name                 = "web-vmss-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
+resource "azurerm_subnet" "smlee_web_subnet" {
+  name                 = "smlee-web-subnet"
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
   address_prefixes     = ["10.0.10.0/28"]
 }
 
 # WAS LB vmss subnet
-resource "azurerm_subnet" "WAS_LB_Subnet" {
-  name                 = "was-lb-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
+resource "azurerm_subnet" "smlee_lb_subnet" {
+  name                 = "smlee-lb-subnet"
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
   address_prefixes     = ["10.0.2.0/29"]
 }
 
 # WAS vmss subnet
-resource "azurerm_subnet" "WAS_Vmss_Subnet" {
-  name                 = "was-vmss-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
+resource "azurerm_subnet" "smlee_was_subnet" {
+  name                 = "smlee-was-subnet"
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
   address_prefixes     = ["10.0.20.0/28"]
 }
 
 # DB subnet
-resource "azurerm_subnet" "DB_Subnet" {
+resource "azurerm_subnet" "smlee_db_subnet" {
   name                 = "db-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
   address_prefixes     = ["10.0.3.0/29"]
   enforce_private_link_endpoint_network_policies = true
 }
 
 # Junp BoX vmss subnet
-resource "azurerm_subnet" "JumpBox_subnet" {
+resource "azurerm_subnet" "jumpbox_subnet" {
   name                 = "jumpbox-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.Azure1_rg.name
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
+  resource_group_name  = azurerm_resource_group.smlee_rg.name
   address_prefixes     = ["10.0.4.0/29"]
 }
 
-resource "azurerm_subnet" "bs_sub" {
+resource "azurerm_subnet" "bs_subnet" {
   name = "AzureBastionSubnet"
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  virtual_network_name = azurerm_virtual_network.smlee_vnet.name
   address_prefixes = ["10.0.5.0/24"]
-
   }
