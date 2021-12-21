c29cea088b852c86c2f81aebbfa2bf06ceaf317e
errormin <smgm030@gmail.com>
2021-12-21 18:30:00 +0900

    2021-12-21 stap6

---
+++ 11_public_ip.tf
@@ -0,0 +1,17 @@
+resource "azurerm_public_ip" "smlee_appgw_pubip" {
+  name                = "smlee-appgw-pubip"
+  location            = azurerm_resource_group.smlee_rg.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  allocation_method   = "Static" #정적 할당
+  domain_name_label   = random_string.fqdn.result
+  tags                = var.tags
+}
+/*
+resource "azurerm_public_ip" "smlee_jumpbox_pubip" {
+  name                = "mlee-jumpbox-pubip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  allocation_method   = "Static"
+  domain_name_label   = "${random_string.fqdn.result}-ssh"
+  tags                = var.tags
+}*/
