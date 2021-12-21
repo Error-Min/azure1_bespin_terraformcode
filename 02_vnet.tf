207601af7b15cb3740668d188d6385a193122e95
errormin <smgm030@gmail.com>
2021-12-21 18:28:19 +0900

    2021-12-21 stap6

--- 02_vnet.tf
+++ 02_vnet.tf
@@ -1,8 +1,7 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vmss-vnet"
+resource "azurerm_virtual_network" "smlee_vnet" {
+  name                = "smlee-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.Azure1_rg.location
-  resource_group_name = azurerm_resource_group.Azure1_rg.name
+  location            = azurerm_resource_group.smlee_rg.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
   tags                = var.tags # (3)
 }
-#===================================================
