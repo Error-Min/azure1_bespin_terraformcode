a9e6caf40741847df606a4d287f3fc6972f02822
errormin <smgm030@gmail.com>
2021-12-22 19:53:25 +0900

    2021-12-22 stap7

--- 08_mysqldb.tf
+++ 08_mysqldb.tf
@@ -2,11 +2,11 @@ resource "azurerm_mysql_server" "smlee_mysql" {
   name                = "smlee-mysql"
   location            = azurerm_resource_group.smlee_rg.location
   resource_group_name = azurerm_resource_group.smlee_rg.name
-
   administrator_login          = var.admin_user
   administrator_login_password = var.admin_password
-
   sku_name   = "GP_Gen5_2"
+
+
   storage_mb = 5120
   version    = "5.7"
   ssl_enforcement_enabled           = false
@@ -67,3 +67,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "smlee_dns_link" {
   private_dns_zone_name = azurerm_private_dns_zone.smlee_private_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.smlee_vnet.id
 }
+
+#=================================================================================
