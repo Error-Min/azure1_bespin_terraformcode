9e26349571851d445b8fe1ef884053e603e316e7
errormin <smgm030@gmail.com>
2021-12-22 19:54:43 +0900

    2021-12-22 stap7 mysql test var

---
+++ 14_mysqldbtest.tf
@@ -0,0 +1,73 @@
+resource "azurerm_private_dns_zone" "smlee_private_dns_zone" {
+  name                = "privatelink.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "smlee_dns_link" {
+  name                  = "smlee-dns-link"
+  resource_group_name   = azurerm_resource_group.smlee_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.smlee_private_dns_zone.name
+  virtual_network_id    = azurerm_virtual_network.smlee_vnet.id
+}
+
+resource "azurerm_mysql_flexible_server" "smlee_mysql" {#
+  name                = "smlee-mysql"
+  location            = azurerm_resource_group.smlee_rg.location
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  administrator_login          = var.admin_user
+  administrator_password      = var.admin_password
+  delegated_subnet_id    = azurerm_subnet.smlee_db_subnet.id
+  private_dns_zone_id    = azurerm_private_dns_zone.smlee_private_dns_zone.id
+  sku_name   = "GP_Standard_D2ds_v4"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.smlee_dns_link]
+}
+
+
+resource "azurerm_mysql_flexible_server_configuration" "time_zone" {#
+  name                = "time_zone"
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  server_name         = azurerm_mysql_flexible_server.smlee_mysql.name
+  value               = "-09:00"
+}
+
+
+resource "azurerm_mysql_flexible_database" "petclinic" { #
+  name                = "petclinic"
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  server_name         = azurerm_mysql_flexible_server.smlee_mysql.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+
+
+/*resource "azurerm_mysql_firewall_rule" "mysqlfw" {
+  name                = "smlee_mysqlfw"
+  resource_group_name = azurerm_resource_group.smlee_rg.name
+  server_name         = azurerm_mysql_server.smlee_mysql.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}*/
+
+
+
+/*resource "azurerm_private_endpoint" "smlee_private_endpoint" {
+    name = "DBendpoint1"
+    location = azurerm_resource_group.smlee_rg.location
+    resource_group_name = azurerm_resource_group.smlee_rg.name
+    subnet_id = azurerm_subnet.smlee_db_subnet.id
+
+    private_service_connection {
+        name = "smt-privateserviceconnection"
+        private_connection_resource_id = azurerm_mysql_flexible_server.smlee_mysql.id
+        is_manual_connection = false
+        subresource_names = [ "mysqlServer" ]
+    }
+
+    private_dns_zone_group {
+        name = "smlee-mysql-mysql-database-azure-com"
+        private_dns_zone_ids = [ azurerm_private_dns_zone.smlee_private_dns_zone.id ]
+}
+}*/
+
+
+#=================================================================================
