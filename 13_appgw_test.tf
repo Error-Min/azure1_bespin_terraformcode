bae359602fbce859687d1bed875a23f91b7b4af9
errormin <smgm030@gmail.com>
2021-12-23 19:35:46 +0900

    2021-12-22 stap8 mysql test var

--- 13_appgw_test.tf
+++ 13_appgw_test.tf
@@ -2,11 +2,11 @@ resource "azurerm_application_gateway" "smlee_web_appgw" {
   name                = "smlee-web-appgw"
   resource_group_name = azurerm_resource_group.smlee_rg.name
   location            = azurerm_resource_group.smlee_rg.location
-  zones = ["1", "2", "3"]
+  #zones = ["1", "2", "3"] 웹에는 22랑 443 점프는 22만 와스는 8009만
 
   sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
+    name = "Standard_v2"
+    tier = "Standard_v2"
   }
 
   autoscale_configuration {
@@ -15,13 +15,17 @@ resource "azurerm_application_gateway" "smlee_web_appgw" {
   }
 
   gateway_ip_configuration {
-    name      = "alb-conf"
+    name      = "appgw-conf"
     subnet_id = azurerm_subnet.smlee_appgw_subnet.id
   }
 
   frontend_port {
-    name = "frontend"
+    name = "frontend-80"
     port = 80
+  }
+  frontend_port {
+    name = "frontend-443"
+    port = 443
   }
 
   frontend_ip_configuration {
@@ -30,10 +34,10 @@ resource "azurerm_application_gateway" "smlee_web_appgw" {
   }
 
   frontend_ip_configuration {
-    name = "private"
-    subnet_id                       = azurerm_subnet.smlee_appgw_subnet.id
-    private_ip_address_allocation   = "Static"
-    private_ip_address              = "10.0.1.5"
+    name                          = "private"
+    subnet_id                     = azurerm_subnet.smlee_appgw_subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.5"
   }
 
   backend_address_pool {
@@ -41,42 +45,83 @@ resource "azurerm_application_gateway" "smlee_web_appgw" {
   }
 
   backend_http_settings {
-    name                  = "settings"
+    name                                = "settings-80"
     pick_host_name_from_backend_address = true
-    probe_name = "alb-probe"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    path                  = "/"
-    protocol              = "Http"
-    request_timeout       = 20
+    #probe_name                          = "probe-443"
+    cookie_based_affinity               = "Disabled"
+    port                                = 80
+    path                                = "/"
+    protocol                            = "Http"
+    request_timeout                     = 20
+  }
+
+
+  backend_http_settings {
+    name                                = "settings-443"
+    pick_host_name_from_backend_address = true
+    probe_name                          = "probe-443"
+    cookie_based_affinity               = "Disabled"
+    port                                = 443
+    path                                = "/"
+    protocol                            = "Https"
+    request_timeout                     = 20
   }
 
   http_listener {
-    name                           = "listener"
+    name                           = "listener-80"
     frontend_ip_configuration_name = "public-ip"
-    frontend_port_name             = "frontend"
+    frontend_port_name             = "frontend-80"
     protocol                       = "Http"
   }
 
+  http_listener {
+    name                           = "listener-443"
+    frontend_ip_configuration_name = "public-ip"
+    frontend_port_name             = "frontend-443"
+    protocol                       = "Https"
+    ssl_certificate_name           = "cert"
+  }
+
+  ssl_certificate {
+    name     = "cert"
+    data     = filebase64("certificate.pfx")
+    password = "1234"
+  }
+
   probe {
-    name = "alb-probe"
-    path = "/"
-    protocol = "Http"
-    interval = 30
-    timeout = 30
-    unhealthy_threshold = 3
+    name                                      = "probe-443"
+    path                                      = "/"
+    protocol                                  = "Https"
+    interval                                  = 30
+    timeout                                   = 30
+    unhealthy_threshold                       = 3
     pick_host_name_from_backend_http_settings = true
     //host = "contoso.com"
 
     match {}
   }
 
+  redirect_configuration {
+    name                 = "redirect"
+    redirect_type        = "Permanent"
+    include_path         = true
+    include_query_string = true
+    target_listener_name = "listener-443"
+  }
+
   request_routing_rule {
-    name                       = "rule"
+    name                       = "rule-443"
     rule_type                  = "Basic"
-    http_listener_name         = "listener"
+    http_listener_name         = "listener-443"
     backend_address_pool_name  = "backend"
-    backend_http_settings_name = "settings"
+    backend_http_settings_name = "settings-80"
+  }
+
+  request_routing_rule {
+    name                        = "rule-80"
+    rule_type                   = "Basic"
+    http_listener_name          = "listener-80"
+    redirect_configuration_name = "redirect"
   }
 }
 
@@ -152,7 +197,7 @@ resource "azurerm_monitor_autoscale_setting" "smlee_web_auto" {
 
     capacity {
       default = 2
-      minimum = 2
+      minimum = 1
       maximum = 7
     }
 
@@ -165,7 +210,7 @@ resource "azurerm_monitor_autoscale_setting" "smlee_web_auto" {
         time_window        = "PT5M"
         time_aggregation   = "Average"
         operator           = "GreaterThan"
-        threshold          = 2
+        threshold          = 65
       }
 
       scale_action {
@@ -185,7 +230,7 @@ resource "azurerm_monitor_autoscale_setting" "smlee_web_auto" {
         time_window        = "PT5M"
         time_aggregation   = "Average"
         operator           = "LessThan"
-        threshold          = 1
+        threshold          = 30
       }
 
       scale_action {
